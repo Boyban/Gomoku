10af0e778f1d16e2c81df96bb05e41f5a332332c
guillaume <guillaume.dhaine@epitech.eu>
2018-10-24 20:43:22 +0200

    [ADD] basics

--- gomoku.py
+++ gomoku.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import sys
+
+class ai:
+
+    def __init__(self, size):
+        self.board_size = size
+        self.board = []
+
+
+def cmd_handling(cmd):
+    try:
+        if cmd.split(' ')[5:] == "START":
+            print("start")
+    except ValueError:
+        print("NIKOUMOUK TU MENVOIE DLA KOBA LA D")
+
+def main():
+    sys.exit(0)
+
+if __name__ == "__main__":
+    main()
