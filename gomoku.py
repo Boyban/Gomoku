9d4b8620440055cb77886eb074bd2aa9f4a79e12
guillaume <guillaume.dhaine@epitech.eu>
2018-11-08 18:37:40 +0100

    [FIX] parsing param command

--- gomoku.py
+++ gomoku.py
@@ -8,16 +8,30 @@ class ai:
 
 def start_function(cmd):
     if len(cmd) != 2:
-        print("UNKNOW")
-    print("start_function")
+        print("ERROR bad START parameter")
+    else:
+        try:
+            int(cmd[1])
+        except ValueError:
+            print("ERROR bad START parameter")
+            return
+        print("OK")
 
 def begin_function():
     print("begin_function")
 
 def turn_function(cmd):
     if len(cmd) != 3:
-        print("UNKNOW")
-    print("turn_function")
+        print("ERROR bad coordinates")
+    else:
+        try:
+            int(cmd[1])
+            int(cmd[2])
+        except ValueError:
+            print("ERROR bad coordinates")
+            return
+        print(cmd)
+        print("turn_function")
 
 def board_function():
     print("board_function")
@@ -25,7 +39,15 @@ def board_function():
 def info_function(cmd):
     if len(cmd) != 3:
         print("UNKNOW")
-    print("info_function")
+    else:
+        try:
+            int(cmd[1])
+            int(cmd[2])
+        except ValueError:
+            print("UNKNOW")
+            return
+        print(cmd)
+        print("info_function")
 
 def end_function():
     print("end_function")
@@ -34,27 +56,27 @@ def about_function():
     print("about_function")
 
 def cmd_handling(line):
-    no_param_dic = { "BEGIN" : begin_function,
-    "BOARD" : board_function, 
-    "END" : end_function,
-    "ABOUT" : about_function }
+    no_param_dic = {
+        "BEGIN" : begin_function,
+        "BOARD" : board_function, 
+        "END" : end_function,
+        "ABOUT" : about_function }
 
-    param_dic = { "START" : start_function,
-    "TURN" : turn_function,
-    "INFO" : info_function }
+    param_dic = {
+        "START" : start_function,
+        "TURN" : turn_function,
+        "INFO" : info_function }
 
     try:
         cmd = line.split(' ')
-        if cmd[0].upper() in no_param_dic and len(cmd) != 1:
-            print("UNKNOW")
-        elif cmd[0].upper() in no_param_dic:
+        if cmd[0].upper() in no_param_dic:
             no_param_dic[cmd[0].upper()]()
-        if cmd[0].upper() in param_dic and (len(cmd) < 2 or len(cmd) > 3):
-            print("UNKNOW")
         elif cmd[0].upper() in param_dic:
             param_dic[cmd[0].upper()](cmd)
+        else:
+            print("UNKOWN command")
     except ValueError:
-        print("NIKOUMOUK TU MENVOIE DLA KOBA LA D")
+        print(ValueError)
         sys.exit(84)
 
 def main():
